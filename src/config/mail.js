c455b1d222402a79133befc85003b8a38c0c8428
Rodolfo Santos Silveira <rodolfo360tisilveira@gmail.com>
2019-12-07 12:29:02 -0300

    enviando email

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'cd6789508510af',
+    pass: '1cccb0e002d27c',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
